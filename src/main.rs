ac65dadecdce38c8f1fb0fe00a860d0ce28baace
Connor Norton <connorn314@gmail.com>
2023-01-24 12:08:42 -0800

    numeric operations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("The value of difference is: {difference}");
+    println!("The value of product is: {product}");
+    println!("The value of truncated is: {truncated}");
+    println!("The value of quotient is: {quotient}");
+    println!("The value of remainder is: {remainder}");
+
 }
