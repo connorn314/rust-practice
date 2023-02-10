454cfba3060a5165e6362891a2c665ccb298089a
Connor Norton <connorn314@gmail.com>
2023-02-10 09:27:32 -0800

    generic types and traits

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
-fn main() {
-    let s: String = String::from("abcdefghijklmnopqrstuvwxyz");
-    for char in s.bytes(){
-        println!("{char}")
+use std::cmp::PartialOrd as cmpPartialOrd;
+
+fn largest<T: cmpPartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
     }
 
-    // let third: i32 = &v[2];
-    // println!("The third element is {third}");
-
-    // let third: Option<&i32> = v.get(2);
-    // match third {
-    //     Some(third) => println!("The third element is {third}"),
-    //     None => println!("There is no third element."),
-    // }
-    let v = vec![1, 2, 3, 4, 5];
-    let first = &v[0];
-
-    // v.push(6);
-
-    println!("The first element is: {first}");
-    // let does_not_exist = &v[100];
-    let does_not_exist = v.get(3);
-    match does_not_exist {
-        Some(num) => println!("The 100th element is {num}"),
-        None => println!("There is no 100th element."),
-    }
+    largest
 }
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
+
